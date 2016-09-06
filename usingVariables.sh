ac6892d47618710d0d8bb9eb258c2c5a5d14f01e
hunkar <hunkarpurtul@gmail.com>
2016-09-06 17:56:14 +0300

    First commit to scripts

---
+++ usingVariables.sh
@@ -0,0 +1,26 @@
+#07.09.2016
+#Hünkar PURTUL
+#www.hunkarpurtul.blogspot.com 
+#Using some system variables
+
+clear
+
+#Print current username of log in to PC
+echo "Username:  $USER"
+
+#Print logging name 
+echo "Logging name:  $LOGNAME"
+
+#Print Operating System type 
+echo "OS type:  $OSTYPE"
+
+#Print current directory
+echo "Current directory:  $PWD"
+
+#Print our Bash version
+echo "Bash version:  $BASH_VERSION"
+
+#print our shell
+echo "Shell:  $SHELL"
+
+exit 0
