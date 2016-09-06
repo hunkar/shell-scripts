49fb5ed4a9afb4f0221b6d0e33ac37a0bb1d9d13
hunkar <hunkarpurtul@gmail.com>
2016-09-06 18:12:15 +0300

    First commit to scripts

---
+++ udVariables.sh
@@ -0,0 +1,18 @@
+#07.09.2016
+#Hünkar PURTUL
+#www.hunkarpurtul.blogspot.com 
+#Using some user defined variables
+
+
+clear
+
+myName=hunkar
+myOS=linux
+year=2016
+
+
+echo "Hello my name is $myName"
+echo "I love $myOS"
+echo "This year is $year"
+
+exit 0
