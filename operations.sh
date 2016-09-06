5a4a64a91c493b1eebb42e9cd90cbe3cf3c8bc40
hunkar <hunkarpurtul@gmail.com>
2016-09-06 18:23:51 +0300

    First commit to scripts

---
+++ operations.sh
@@ -0,0 +1,20 @@
+#07.09.2016
+#Hünkar PURTUL
+#www.hunkarpurtul.blogspot.com 
+#Using operations with variables
+
+
+clear
+
+num1=65
+num2=16
+echo "num1 = $num1"
+echo "num2 = $num2"
+echo "num1 - num2 = `expr $num1 - $num2`"
+echo "num1 + num2 = `expr $num1 + $num2`"
+echo "num1 \* num2 = `expr $num1 \* $num2`"
+echo "num1 / num2 = `expr $num1 / $num2`"
+echo "num1 % num2 = `expr $num1 % $num2`"
+
+
+exit 0
